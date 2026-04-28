00e211aec4e45d15040facfb87b7b706c97559e8
cesareye <cesar.arvelo@escola.pr.gov.br>
2026-04-28 12:12:41 -0300

    initBranch

--- asa.js
+++ asa.js
@@ -1,17 +1,35 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
 const porta = 8080;
 
 app.use(express.urlencoded({ extended: true }));
-app.use (express.json());
+app.use(express.json());
 
-app.post ('/finalizar-venda', (req,res) =>{
-    let proreProduto = req.body.procureProduto
+app.post('/finalizar-venda', (req, res) => {
+    let produto = req.body.produto
     let procoUnitario = req.body.precoUnitario
     let quantidade = req.body.quantidade
     let cupomDesconto = req.body.cupomDesconto
 
-    if (quantidade <= 0){
-        return 
+    if (quantidade <= 0) {
+        return res.status(400).json({
+            sucesso: false,
+            erro: "Quantidade invalidada!"
+        })
     }
+
+    valorTotal = quantidade * procoUnitario
+
+    if (cupomDesconto === "PROMO20") {
+        valorTotal = valorTotal * 0.80;
+    }
+
+    return res.status(201).json({
+        sucesso: true,
+        mensagem: `seu ${produto} comprado com sucesso! valor ${valorTotal}`
+    })
+
+})
+app.listen(porta, () => {
+    console.log(`Servidor rodando! Agurdando o formularrio HTML na porta ${porta}...`)
 })
